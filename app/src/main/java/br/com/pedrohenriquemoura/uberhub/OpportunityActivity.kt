75227f37aa0d391f1c1065e94f978d62cca26de8
Pedro Moura <pedromoura@MacBook-Pro-de-Pedro.local>
2020-11-16 23:43:13 -0300

    Implementation initial layout flow signin

--- app/src/main/java/br/com/pedrohenriquemoura/uberhub/OpportunityActivity.kt
+++ app/src/main/java/br/com/pedrohenriquemoura/uberhub/OpportunityActivity.kt
@@ -10,6 +10,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
 
 
 class OpportunityActivity : AppCompatActivity() {
